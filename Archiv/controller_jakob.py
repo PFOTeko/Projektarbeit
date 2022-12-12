4d74442a0353ec699b0236b6e5c84cef09fa12fb
oliver <oliver.pfenniger@edu.teko.ch>
2022-12-12 19:14:29 +0100

    add to git

---
+++ Archiv/controller_jakob.py
@@ -0,0 +1,34 @@
+from Game import gui
+from Game import field
+
+
+class Controller:
+
+    def __init__(self):
+        self.gui = gui.Gui()
+        self.gui.register_callback(self.gui_callbacks)
+
+        #self.field = field.Field()
+
+    def start_gui(self):
+        self.gui.show_window()
+
+    def gui_callbacks(self, event, data):
+        print("gui_callbacks: " + str(event))
+
+    def model(self):
+
+        print('Hallo Welt')
+
+    def pressed(event):
+        pressed = event.keysym
+        keys = ['Up', 'Down', 'Right', 'Left']
+
+        if pressed in keys:
+            Controller.model(pressed)
+            print(event.keysym)
+
+
+if __name__ == "__main__":
+    start = Controller()
+    start.start_gui()
--- Archiv/gui_jakob.py
+++ Archiv/gui_jakob.py
@@ -1,22 +1,29 @@
 import tkinter as tk
 import controller as c
+from observable import Observable
 
 
-class Gui:
+class Gui(Observable):
 
     def __init__(self):
 
-        self.callback = c.Controller()
         self.window = tk.Tk()
         self.window.title("Snake")
         self.window.geometry("600x800")
         self.window.resizable(width=False, height=False)
-        self.window.bind('<Key>',  self.callback.pressed)
+        self.window.bind('<Key>',  c.Controller.pressed) #statische implementierung
+        #self.window.bind('<Key>', self.pressed)
         self.spielfeld()
+
 
     def show_window(self):
         self.window.mainloop()
 
+    '''
+    def pressed(self, event):
+        self._notify("gui_pressed", "my event" )
+        pass
+    '''
     def spielfeld(self):
 
         beige = '#F5F5DC'
